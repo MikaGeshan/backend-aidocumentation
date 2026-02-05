6fa0ccbba710810702e8a6c702698be21b3cb886
mika.arip <mika.arip@quadrant-si.id>
2026-02-05 21:52:13 +0700

    fix wrong imports

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
-import { GoogleDriveService } from 'src/drive/drive.service';
-import { QdrantService } from 'src/qdrant/qdrant.service';
-import { LlmService } from 'src/llm/llm.service';
+import { GoogleDriveService } from '../drive/drive.service';
+import { QdrantService } from '../qdrant/qdrant.service';
+import { LlmService } from '../llm/llm.service';
 
 @Module({
   controllers: [DocumentsController],
